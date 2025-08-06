6be90a1b7040d9bf64a1f1566e4541f1e7e8498a
xinoodev <errxino.soporte@gmail.com>
2025-08-06 02:04:55 +0200

    Updated config files

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const monk = require('monk');
+require('dotenv').config();
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+
+urls.createIndex({ slug: 1 }, { unique: true });
+
+module.exports = {
+    db,
+    urls,
+};
